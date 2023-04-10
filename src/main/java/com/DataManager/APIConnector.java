11c7a263cb07b5d4e7ce0a394223abc7ae19778d
phanhieu244 <phanhieu2443@gmail.com>
2023-04-10 16:27:21 +0700

    Update pom.xml, API example

---
+++ src/main/java/com/DataManager/APIConnector.java
@@ -0,0 +1,14 @@
+package com.DataManager;
+
+import java.net.MalformedURLException;
+
+public class APIConnector {
+
+    private String urlString;
+
+    public APIConnector(String urlString) throws MalformedURLException{
+        this.urlString = urlString;
+    }
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.quiz {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     exports com.quiz;
     opens com.quiz to javafx.fxml;
