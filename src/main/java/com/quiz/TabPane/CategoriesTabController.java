c613fca3b026ce8b183f376933bf4e635349cf4d
phanhieu244 <phanhieu2443@gmail.com>
2023-04-19 17:49:48 +0700

    import UI

--- src/main/java/com/quiz/TabPane/CategoriesTabController.java
+++ src/main/java/com/quiz/TabPane/CategoriesTabController.java
@@ -4,16 +4,12 @@ import com.DataManager.CategoryAPI;
 import com.quiz.TabPane.QuestionTab.QuestionTabController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class CategoriesTabController {
     @FXML
--- src/main/java/com/quiz/TabPane/ImportTabController.java
+++ src/main/java/com/quiz/TabPane/ImportTabController.java
@@ -1,39 +1,74 @@
 package com.quiz.TabPane;
 
-import com.DataManager.APIConnector;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import okhttp3.*;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.apache.poi.ss.formula.functions.Choose;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
 public class ImportTabController implements Initializable {
-    @FXML
-    public Label string;
-    @FXML
-    public TextField textField;
-    @FXML
-    private Button button;
-    @FXML
-    private Button get;
-    @FXML
-    private TextArea textArea;
 
+    @FXML
+    private Button choose;
+
+    @FXML
+    private Button importBut;
+
+    private File file;
+
+    @FXML
+    private void chooseFile(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        file = fileChooser.showOpenDialog(null);
+    }
+
+    @FXML
+    private void importFile(ActionEvent event) {
+        if (file == null) {
+            System.out.println("File is not found");
+            return;
+        }
+        if (!CheckFileText(file.getName())) {
+            showWarning();
+            return;
+        }
+        showMessageBox();
+    }
+
+    private void showWarning(){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("WARNING");
+        alert.setContentText("Wrong Format");
+        alert.showAndWait();
+    }
+
+    private void showWrongAiken(int i){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("WARNING");
+        alert.setContentText("Error at" + i);
+        alert.showAndWait();
+    }
+
+    private void showMessageBox(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("OK");
+        alert.setTitle("Message Box");
+        alert.showAndWait();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        //fileChooser.getExtensionFilters().add
     }
 
-
+    private boolean CheckFileText(String fileName){
+        return fileName.endsWith(".txt") || fileName.endsWith(".doc") || fileName.endsWith(".docx");
+    }
 }
